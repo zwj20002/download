cd2de031f1af2d8823dbe8f0cda5c0b6d114bfc4
zwj <3086526860@qq.com>
2025-05-08 11:49:13 +0800

    Updates

---
+++ assets/en-BnRh-MzW.js
@@ -0,0 +1,5 @@
+const o="Light Year Service Center",n="Light Year Service Center",t="Click to download the Light Year APP",e="Light Year also supports",a="Android, iOS, Android 32-bit, Google Play version",d="Make sure your app is up to date to get the latest features",i=`iOS
+Download`,r=`Android
+Download`,s=`Android 32bit
+Download`,c=`GooglePlay
+Download`,l="Join the official fan group chat to explore more interesting content",g="Join the official fan chat",p={pageTitle:o,project:n,download:t,desc_one:e,desc_two:a,desc_three:d,ios_download:i,android_download:r,android_32bit_download:s,android_google_play_download:c,join:l,group:g};export{s as android_32bit_download,r as android_download,c as android_google_play_download,p as default,e as desc_one,d as desc_three,a as desc_two,t as download,g as group,i as ios_download,l as join,o as pageTitle,n as project};
